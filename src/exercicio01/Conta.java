328ea5cb965c739deb044e80abc6b17dcbd52b25
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-24 09:41:12 -0300

    Exercicio 01 feito

--- src/exercicio01/Conta.java
+++ src/exercicio01/Conta.java
@@ -6,4 +6,17 @@ public class Conta {
     int nmr;
     double saldo;
 
+
+
+    //metodo para depositar um valor na conta
+    public void depositar(double valor){
+        saldo = saldo + valor;
+    }
+
+    //metodo para sacar um valor da conta
+
+    public void sacar(double valor){
+        saldo = saldo - valor;
+    }
+
 }
---
+++ src/exercicio01/Main.java
@@ -0,0 +1,27 @@
+package exercicio01;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        Conta conta = new Conta();
+        double valor;
+
+        System.out.println("Nome do correntista --> ");
+        conta.correntista = sc.next();
+        conta.nmr = rd.nextInt(1000,9999);
+
+        System.out.println("Saldo inicial: R$ "+conta.saldo);
+        System.out.println("Informe o valor para depósito: R$ ");
+        valor = sc.nextDouble();
+        conta.depositar(valor);
+        System.out.println("Informe o valor para saque: R$ ");
+        valor = sc.nextDouble();
+        conta.sacar(valor);
+        System.out.println("Saldo atual: R$ "+conta.saldo);
+
+    }
+}
